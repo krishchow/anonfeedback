e9b3c6aad233cc83038b51cfed0d87f709e530e7
Andrew Petersen <andrew.petersen@utoronto.ca>
2018-04-13 23:40:22 -0400

    Update to post data that restricts responses to Petersen

--- responses/data/scripts/get_anonfeedback_data.sh
+++ responses/data/scripts/get_anonfeedback_data.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# To be run on mcs. Assumes all posts have a subject including "Feedback Responses"
-mysql mcs_bb -u peters43 -p <<< "select * from mcs_bb_posts where subject like '%Feedback Responses%';" > posts.tsv
+# To be run on mcs. Assumes all posts have a subject including "Feedback Responses" and are written by Petersen.
+mysql mcs_bb -u peters43 -p <<< "select * from mcs_bb_posts where subject like '%Feedback Responses%' and username='peters43';" > posts.tsv
